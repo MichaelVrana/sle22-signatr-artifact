64bbabc22d4776c98d05d24777a060aff1a84786
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2022-10-17 16:04:08 +0200

    Retrieve type distribution

---
+++ type-distribution.R
@@ -0,0 +1,39 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(dplyr))
+
+library(sxpdb)
+
+compute_type_distribution <- function() {
+  db <- open_db("data/cran_db", q=F)
+  
+  q_int <- query_from_plan(type = 3L)
+  q_list <- query_from_plan(type = list(4))
+  q_logical <- query_from_plan(type = TRUE)
+  q_real <- query_from_plan(type = 3)
+  q_string <- query_from_plan(type = "hello world")
+  
+  n_int <- nb_values_db(db, q_int)
+  n_list <- nb_values_db(db, q_list)
+  n_logical <- nb_values_db(db, q_logical)
+  n_real <- nb_values_db(db, q_real)
+  nb_values <- size_db(db)  
+  
+  df <- tribble(~type, ~n, ~pc,
+                "Integer", n_int, n_int / nb_values * 100,
+                "List", n_list, n_list / nb_values * 100,
+                "Logical", n_logical, n_logical / nb_values * 100,
+                "Others", (nb_values - (n_int + n_list + n_logical + n_string + n_real)) / nb_values * 100,
+                "Real", n_real, n_real / nb_values * 100,
+                "String", n_string, n_string / nb_values * 100
+  )
+  
+  qs::qsave(df, "data/type-distribution.qs")
+  
+  return(df)
+}
+
+if(sys.nframe() == 0L) {
+  compute_type_distribution
+}
+
